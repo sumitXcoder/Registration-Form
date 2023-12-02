fa9e4bc8d40aa3b517c06d8e5e27c3bb4df230d6
sumitXcoder <sumitkumar230404@gmail.com>
2023-12-02 07:34:42 +0530

    fix recovery bug

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -200,6 +200,7 @@ export async function updateUser(req, res) {
 
 /** GET: http://localhost:8080/api/generateOTP */
 export async function generateOTP(req, res) {
+
     req.app.locals.OTP = await otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false })
     res.status(201).send({ code: req.app.locals.OTP })
 }
--- server/controllers/mailer.js
+++ server/controllers/mailer.js
@@ -1,63 +1,28 @@
 import nodemailer from 'nodemailer';
-import Mailgen from 'mailgen';
 import dotenv from "dotenv";
 dotenv.config();
 
-// https://ethereal.email/create
-let nodeConfig = {
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
-        user: process.env.EMAIL, // generated ethereal user
-        pass: process.env.PASSWORD, // generated ethereal password
-    }
-}
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    },
+});
 
-let transporter = nodemailer.createTransport(nodeConfig);
 
-let MailGenerator = new Mailgen({
-    theme: "default",
-    product : {
-        name: "Mailgen",
-        link: 'https://mailgen.js/'
-    }
-})
-
-/** POST: http://localhost:8080/api/registerMail 
- * @param: {
-  "username" : "example123",
-  "userEmail" : "admin123",
-  "text" : "",
-  "subject" : "",
-}
-*/
 export const registerMail = async (req, res) => {
-    const { username, userEmail, text, subject } = req.body;
-
-    // body of the email
-    var email = {
-        body : {
-            name: username,
-            intro : text || 'Welcome to Daily Tuition! We\'re very excited to have you on board.',
-            outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
-        }
-    }
-
-    var emailBody = MailGenerator.generate(email);
-
-    let message = {
-        from : process.env.EMAIL,
+    const { userEmail, text, subject } = req.body;
+    const mail = {
+        from: process.env.EMAIL,
         to: userEmail,
-        subject : subject || "Signup Successful",
-        html : emailBody
-    }
+        subject: subject,
+        html: text,
+    };
 
-    // send mail
-    transporter.sendMail(message)
+    transporter.sendMail(mail)
         .then(() => {
-            return res.status(200).send({ msg: "You should receive an email from us."})
+            return res.status(200).send({ msg: "You should receive an email from us." })
         })
-        .catch(error => res.status(500).send({ error }))
-
-}
+        .catch(error => res.status(500).send(error))
+};
